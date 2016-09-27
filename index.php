2bb5501e9ad72ef65e6c03570ddf7f3cc919b82f
Julian da Costa Faro <juliandacostafaro@hotmail.com>
2016-09-27 13:44:47 -0300

    Subo avances: Funciona el listado pidiendo permisos

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+require_once 'google-api-php-client/autoload.php';
+
+session_start();
+
+$client = new Google_Client();
+$client->setAuthConfigFile('client_secrets.json');
+$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
+
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']){
+  $client->setAccessToken($_SESSION['access_token']);
+  $drive_service = new Google_Service_Drive($client);
+	$optParams = array(
+  		'pageSize' => 10,
+ 		 'fields' => 'nextPageToken, files(id, name)'
+	);
+	$results = $drive_service->files->listFiles($optParams);
+
+	if (count($results->getFiles()) == 0) {
+	  print "No files found.\n";
+	} else {
+	  print "Files:\n<br>";
+	  foreach ($results->getFiles() as $file) {
+	    printf("%s (%s)\n", $file->getName(), $file->getId()); echo"<br>";
+	  }
+	}
+
+} else {
+  $authUrl = $client->createAuthUrl();
+  print "<a class='login' href='$authUrl'>Conectar</a>";	
+  //$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/grupo10/oauth2callback.php';
+  //header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+}
